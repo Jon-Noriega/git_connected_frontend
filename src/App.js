4fcb4d6825d2ee3db1995e061f881df3ec825a31
bgordon8 <gordonbrian16@gmail.com>
2020-10-14 13:52:53 -0600

    Adds template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
-import Login from './components/Login'
+import React from 'react'
+import './App.css'
+import Programs from './components/Programs'
 
-class App extends Component {
+class App extends React.Component {
 
   state = {
-    user: '',
+    programs: [],
+    schedule: [],
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    console.log(event)
-}
 
-  render(){
+  addToSchedule = (program) => {
+    if (!this.state.schedule.find(aliveCharacter => aliveCharacter === program)) {
+      const updatedSchedule = this.state.programs.filter(prog => prog !== program)
+      this.setState({ programs: updatedSchedule, schedule: [...this.state.schedule, program] })
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://breakingbadapi.com/api/programs')
+      .then(response => response.json())
+      .then(programs => this.setState({ programs }))
+  }
+
+  render() {
     return (
       <div className="App">
-          <Login user={this.state.user} handleSubmit={this.handleSubmit}/>
+        <h1>Breaking Bad</h1>
+        <main>
+          <section className='all-programs'>
+            <h2>All Characters</h2>
+            <Programs
+              programs={this.state.programs}
+              addToAlive={this.addToAlive}
+            />
+          </section>
+          <div className='sorted-programs'>
+            <section className='schedule'>
+              <h2> Alive</h2>
+              <Programs
+                programs={this.state.schedule}
+                addToAlive={this.addToAlive}
+
+              />
+            </section>
+            <section className='deceased'>
+              <h2>Deceased</h2>
+            </section>
+          </div>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
